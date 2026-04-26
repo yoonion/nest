03d02aa3f1fe937c99d088e5c55614b1c7b13a37
yoonion <yoonion97@gmail.com>
2026-04-26 17:41:23 +0900

    chore: add baseline migration

---
+++ src/database/migrations/1777192867005-BaselineInit.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class BaselineInit1777192867005 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
